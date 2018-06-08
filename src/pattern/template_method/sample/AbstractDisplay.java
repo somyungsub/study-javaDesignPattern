90854652a079d04ec9cd194f7680d71621452b43
somyungsub <gkdldy5@naver.com>
2018-06-08 22:44:31 +0900

    Add : 템플릿메서드 패턴 주석추가

--- src/pattern/template_method/sample/AbstractDisplay.java
+++ src/pattern/template_method/sample/AbstractDisplay.java
@@ -3,11 +3,15 @@ package pattern.template_method.sample;
 /*
     상위클래스
     - 하위클래스에 Template 제공
+    - 흐름(뼈대)을/를 제공 -> display 메서드
  */
 
 public abstract class AbstractDisplay {
 
-    // 템플릿메서드
+    /*
+        템플릿메서드
+        -> final : 하위클래스에서 오버라이딩을 못하게 막음 (재정의가 이루어지면 안됨)
+     */
     public final void display(){
         // print를 5회 호출하는 프로세스
         open();
